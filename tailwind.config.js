4c4e891acd4228de4718204531eadf45e94b9063
zawadzamil <contact.zawadzamil@gmail.com>
2023-06-08 19:19:44 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            spacing: {
+                12: "48px",
+                17: "5rem",
+            },
+        },
+    },
+    plugins: [],
+};
